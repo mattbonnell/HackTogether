f99cec1a9453c964d11cd48cb750154842d1483f
mattbonnell <matt.bonnell@icloud.com>
2018-03-04 07:06:00 -0500

    Add ProjectDetailView

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('project/<int:project_id>/', views.ProjectDetailView.as_view(), name='project-detail')
 ]
--- app/views.py
+++ app/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.views.generic import DetailView
+from .models import Project
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("Hello")
+    context = {'user': request.user}
+    return render(request, 'app/index.html', context)
+
+
+class ProjectDetailView(DetailView):
+
+    model = Project
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
